f06062aaf663f345941db13167de12becdc8422c
maxime1907 <19607336+maxime1907@users.noreply.github.com>
2023-06-12 13:57:42 +0200

    feat(butane): private interface

--- terraform/ovh-krysantem-gra1-prod/main.tf
+++ terraform/ovh-krysantem-gra1-prod/main.tf
@@ -11,9 +11,9 @@ module "network" {
   bare_metal_byoi = {
     kimsufi = {
       service_name = var.BAREMETAL_0
-      url = "https://stable.release.flatcar-linux.net/amd64-usr/3374.2.5/flatcar_production_openstack_image.img.bz2"
+      url = "https://stable.release.flatcar-linux.net/amd64-usr/3510.2.2/flatcar_production_openstack_image.img.bz2"
       type = "qcow2"
-      description = "Flatcar stable 3374.2.5 amd64"
+      description = "Flatcar stable 3510.2.2 amd64"
       configdrive = {
         enable = true
         user_data = data.ct_config.baremetal.rendered
